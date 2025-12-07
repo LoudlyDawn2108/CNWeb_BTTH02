715f17733818ac26f247f7b62beb479003b45235
yukik <kyokoxdev@gmail.com>
2025-12-07 23:44:02 +0700

    Implemented missing methods into InstructorController, added StudentList view

---
+++ viewmodels/instructor/StudentListViewModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ViewModels\Instructor;
+
+use Lib\ViewModel;
+
+class StudentListViewModel extends ViewModel {
+    public function __construct(
+        public string $title,
+        public array $students,
+        public ?array $course = null
+    ) {}
+}
