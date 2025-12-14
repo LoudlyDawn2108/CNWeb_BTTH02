bb0cd5e1cccec2119337085e54e090d9b848d77d
yukik <kyokoxdev@gmail.com>
2025-12-14 22:33:44 +0700

    Fix: Duplicated CourseView

--- viewmodels/StudentViewModels.php
+++ viewmodels/StudentViewModels.php
@@ -2,14 +2,8 @@
 
 namespace ViewModels;
 
-use Models\Course;
 use Models\Lesson;
 use Models\Material;
-
-class CourseView extends Course {
-    public ?string $category_name = null;
-    public ?string $instructor_name = null;
-}
 
 class LessonView extends Lesson {
     public ?int $material_count = null;
